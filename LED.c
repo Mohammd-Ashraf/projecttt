c51de51925dc2c1830f7b14a955218b7a676e77c
Mohamed Youssef <1901194@eng.asu.edu.eg>
2022-05-11 01:18:34 +0200

    LED

---
+++ LED.c
@@ -0,0 +1,19 @@
+#include "gpio.h"
+
+
+void LED_INIT (unsigned char Portname ,unsigned char Pinnumber){
+	
+	PIN_DIR(Portname,Pinnumber,1);
+}
+
+void LED_ON (unsigned char Portname,unsigned char Pinnumber){
+	
+	PIN_WRITE(Portname,Pinnumber,1);
+
+}
+
+void LED_OFF (unsigned char Portname,unsigned char Pinnumber){
+	
+	PIN_WRITE(Portname,Pinnumber,0);
+
+}
