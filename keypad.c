74f73ebfca43e4a24c9e7d7efb162104945e7d39
Mohamed Youssef <1901194@eng.asu.edu.eg>
2022-05-11 03:28:54 +0200

    Create keypad.c

---
+++ keypad.c
@@ -0,0 +1,27 @@
+#include "gpio.c"
+
+
+unsigned char KEYPAD_READ(Portname){
+	
+	unsigned char arr[4][4] = {
+				{'1', '2', '3', 'A'},
+				{'4', '5', '6', 'B'},
+				{'7', '8', '9', 'C'},
+				{'*', '0', '#', 'D'}
+														};
+char row,col,x;
+char returnvalue=0xFF;
+	for(row=0;row<4;row++){
+	WRITE_LOWERBITS(Portname,0xFF);
+	PIN_WRITE(Portname,row,0);
+	for(col=0;col<=3;col++){
+		x = PIN_READ(Portname, col+4);
+		if(x==0){
+			returnvalue=arr[row][col];
+		break;
+	  }
+		if(x==0) break;
+		return returnvalue;			
+    }
+	}
+}
