5d29b080dd6aaac9d7d6f90943008f3f09918674
Fares Amr <105319701+FaresAmr74@users.noreply.github.com>
2022-05-11 09:52:09 +0200

    Initialize the LCD
    make the first function and the configuring

--- LCD_Driver/main.c
+++ LCD_Driver/main.c
@@ -1,7 +1,70 @@
+#include "gpio.h"
 #include "LCD_Configuration.h"
 
 
-Fares
+
+
+
+
+
+
+/*Function to initialize the LCD */
+void LCD_Int(void){
+
+    PORT_DIR(Data_Port,0xFF);  /*Make all Data port as output */
+
+    PIN_DIR(Data_Port  , Enable_pin ,1);    /*Make the control bits as outputs */
+    PIN_DIR(Data_Port  , ReadWrite_pin ,1);
+    PIN_DIR(Data_Port  , RegisterSelect_pin ,1)
+
+    PIN_WRITE(Control_Port , ReadWrite_pin , 0 );  /*we will use the ReadWrite pin to write */
+
+    LCD_Sending_Command(8_bit_mode);  /*Send the 8-bit data directly in one stroke*/
+    Delay_LCD(1);
+
+    LCD_Sending_Command(Display_ON); /* Display ON, cursor OFF*/
+    Delay_LCD(1);
+
+    LCD_Sending_Command(Clear_Screen); /* Clear the display of the LCD */
+    Delay_LCD(10);
+
+    LCD_Sending_Command(Entry_mode); /* Write the charcter and move the cursor */
+    Delay_LCD(1);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
