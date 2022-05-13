69ccfa722abddae8b067fcec68b391985628142b
Mohamed Youssef <1901194@eng.asu.edu.eg>
2022-05-13 19:18:13 +0200

    Update TIMER.c

--- TIMER.c
+++ TIMER.c
@@ -1,18 +1,22 @@
-void SysTick_Wait(){
+#include "tm4c123gh6pm.h"
+
+
+void SysTick_Wait(void){
 		NVIC_ST_CTRL_R = 0x00;
 		NVIC_ST_RELOAD_R = 0x00FFFFFF;
 		NVIC_ST_CURRENT_R = 0;
 		NVIC_ST_CTRL_R = 0x05 ;
 }
 
-void wait1ms(){
+void wait1ms(void){
 		NVIC_ST_RELOAD_R = 0x3E80-1;
 		NVIC_ST_CURRENT_R = 0;
 	while((NVIC_ST_CTRL_R & 0x00010000)==0);
 }
 
 void genericdelay(unsigned long time){
-	for (int i=0; i<time; i++){
-		SysTick_Wait(a);
+	int i;
+	for (i=0; i<time; i++){
+		wait1ms();
 	}		
 }
