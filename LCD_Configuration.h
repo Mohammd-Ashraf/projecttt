d5ecc2c067edfe3600280ab5825be0411e8db6fe
Fares Amr <105319701+FaresAmr74@users.noreply.github.com>
2022-05-14 11:14:09 +0200

    add prototype

--- LCD_Configuration.h
+++ LCD_Configuration.h
@@ -33,6 +33,12 @@
 #define RegisterSelect_pin 2
 
 
-
+void LCD_Int(void);
+static void Pulse_func(void);
+void LCD_Send_Command(char command);
+void LCD_Send_character(char character);
+void LCD_Send_string(char *data);
+void LCD_clearScreen(void);
+void cursor_move(char row , char column);
 
 #endif // LCD_CONFIGURATION_H_INCLUDED
