29b84d85cda534abb054c9fdf83742205ca5b595
Mohamed Youssef <1901194@eng.asu.edu.eg>
2022-05-15 12:02:42 +0200

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -33,9 +33,9 @@ void LCD_Int(void){
 
     PORT_DIR(Data_Port,0xFF);  /*Make all Data port as output */
 
-    PIN_DIR(Data_Port  , Enable_pin         ,1);    /*Make the control bits as outputs */
-    PIN_DIR(Data_Port  , ReadWrite_pin      ,1);
-    PIN_DIR(Data_Port  , RegisterSelect_pin ,1);
+    PIN_DIR(Control_Port  , Enable_pin         ,1);    /*Make the control bits as outputs */
+    PIN_DIR(Control_Port  , ReadWrite_pin      ,1);
+    PIN_DIR(Control_Port  , RegisterSelect_pin ,1);
 
     PIN_WRITE(Control_Port , ReadWrite_pin , 0 );  /*we will use the ReadWrite pin to write */
 
