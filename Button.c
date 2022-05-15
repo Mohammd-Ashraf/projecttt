7257987c041898a617e4efc6af4bab8de915f3d2
Mohamed Youssef <1901194@eng.asu.edu.eg>
2022-05-15 12:16:52 +0200

    Update Button.c

--- Button.c
+++ Button.c
@@ -1,8 +1,6 @@
-#include "tm4c123gh6pm.h"
-#include "gpio.h"
+#include "GPIO.h"
 
 void BUTTON_INIT (unsigned char Portname , unsigned char Pinnumber){
-	Port_init(Portname);
 	PIN_DIR(Portname , Pinnumber , 0);
 }
 
@@ -12,7 +10,6 @@ unsigned char BUTTON_READ (unsigned char Portname , unsigned char Pinnumber){
 }
 
 void BUTTON_INIT_PULLUP (unsigned char Portname , unsigned char Pinnumber){
-	Port_init(Portname);
 	PIN_DIR(Portname , Pinnumber , 0);
 	PIN_PUR(Portname,Pinnumber,1);
 }
