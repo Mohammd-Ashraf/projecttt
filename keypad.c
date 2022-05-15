e10f102e722f94a9a2faa70894ea421a6c2d7ec5
Mohamed Youssef <1901194@eng.asu.edu.eg>
2022-05-15 12:17:03 +0200

    Update keypad.c

--- keypad.c
+++ keypad.c
@@ -1,17 +1,19 @@
 #include "GPIO.h"
 #include "Button.h"
+#define 	KProw   'E'
+#define   KPcol   'A'
 
-void KEYPAD_INIT(unsigned char Portname){
-	Port_init(Portname);
-	PORT_DIR(Portname,0x0F);
-	PIN_PUR(Portname,4,1);
-	PIN_PUR(Portname,5,1);
-	PIN_PUR(Portname,6,1);
-	PIN_PUR(Portname,7,1);
+void KEYPAD_INIT(void){
+	PORT_DIR(KProw,0xFF);
+	PORT_DIR(KPcol,0x00);
+	PIN_PUR(KPcol,4,1);
+	PIN_PUR(KPcol,5,1);
+	PIN_PUR(KPcol,6,1);
+	PIN_PUR(KPcol,7,1);
 }
 
 
-unsigned char KEYPAD_READ(unsigned char Portname){
+unsigned char KEYPAD_READ(void){
 	
 	unsigned char arr[4][4] = {
 				{'1', '2', '3', 'A'},
@@ -22,16 +24,18 @@ unsigned char KEYPAD_READ(unsigned char Portname){
 char row,col,x;
 char returnvalue=0xFF;
 	for(row=0;row<4;row++){
-	WRITE_LOWERBITS(Portname,0xFF);
-	PIN_WRITE(Portname,row,0);
+	WRITE_LOWERBITS(KProw,0xFF);
+	PIN_WRITE(KProw,row,0);
 	for(col=0;col<=3;col++){
-		x = PIN_READ(Portname, col+4);
+		x = PIN_READ(KPcol, col+4);
 		if(x==0){
 			returnvalue=arr[row][col];
 		break;
 	  }
-		if(x==0) break;
-		return returnvalue;			
+		if(x==0) {
+    break;}
     }
 	}
+			return returnvalue;			
+
 }
