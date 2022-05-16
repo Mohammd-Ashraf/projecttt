bdac93a65ff480e0fea5eccb318a71daa0ac38f7
Mohamed Youssef <1901194@eng.asu.edu.eg>
2022-05-16 23:58:28 +0200

    Update keypad.c

--- keypad.c
+++ keypad.c
@@ -3,7 +3,7 @@
 #include "tm4c123gh6pm.h"
 
 
-unsigned char arr[4][4] = {
+unsigned char arr[4][4] = {                 //better outside the function
 				{'1', '2', '3', 'A'},
 				{'4', '5', '6', 'B'},
 				{'7', '8', '9', 'C'},
@@ -12,13 +12,13 @@ unsigned char arr[4][4] = {
 
 														
  void KEYPAD_INIT(void){
-	Port_init('A');
-  Port_init('D');
-  PORT_DIR('D',0x00);
-  PORT_DIR('A',0xF0);
-	GPIO_PORTD_PDR_R |= 0x0F;
-	 GPIO_PORTD_DATA_R &= 0x0F;
-	 GPIO_PORTA_DATA_R &=0XF0;
+	Port_init('A');                           // init Port A for col
+  Port_init('D');                           // init Port D for rows
+  PORT_DIR('D',0x00);                       // rows >>>> input
+  PORT_DIR('A',0xF0);       								// col  >>>> output
+	GPIO_PORTD_PDR_R |= 0x0F;									// Pull Down (rows)
+	 GPIO_PORTD_DATA_R &= 0x0F;               // ??
+	 GPIO_PORTA_DATA_R &= 0XF0;               // ??
 }
 
 
@@ -29,15 +29,16 @@ unsigned char KEYPAD_READ(void){
 	int i,j;
   while(1)
   {
-    for(i = 0; i < 4; i++)    //Scan columns loop
+    for(i = 0; i < 4; i++)   								 // Scan columns
     {
-      PORT_WRITE('A',(1U << (i+4)));
-			genericdelay(2,1);
-       for( j = 0; j < 4; j++)  //Scan rows
+      PORT_WRITE('A',(1U << (i+4)));         // passing 1 through colums 
+			genericdelay(2,1);       							 // delay 2us
+			
+       for( j = 0; j < 4; j++)  						 // Scan rows
       {
-        if((PORT_READ('D') & 0x0F) & (1U << (j))){
-          GPIO_PORTD_DATA_R &= (~0X0F);
-          GPIO_PORTA_DATA_R &= (~0XF0);
+        if((PORT_READ('D') & 0x0F) & (1U << (j))){         // check if any key is pressed
+          GPIO_PORTD_DATA_R &= (~0X0F);										 // clear Port D (lowerbits)
+          GPIO_PORTA_DATA_R &= (~0XF0);										 // clear Port A (upperbits)
 					return arr[j][i];}
       }
     }
