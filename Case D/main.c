debd79a0481d970b3fc34eb3c2c79e9d4dda249e
Fares Amr <105319701+FaresAmr74@users.noreply.github.com>
2022-05-17 09:24:39 +0200

    Beginning of 'D' Case
    The first part of 'D" case in the main code

---
+++ Case D/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int num1 =0 , num2 =0 ;
+   int num3 =0 , num4 =0 ;
+Case('D');   /*If D is pushed on the keypad, the words “Cooking Time?” should appear on the LCD.*/
+cursor_move(1 ,1);
+LCD_Send_string('Cooking Time?');
+genericdelay(2000, 0);
+
+num1= KEYPAD_READ();
+LCD_clearScreen();
+cursor_move(1 ,1);
+LCD_Send_string('00:0');
+cursor_move(1 ,5);
+LCD_Send_character(num1);
+
+
+num2= KEYPAD_READ();
+LCD_clearScreen();
+cursor_move(1 ,1);
+LCD_Send_string('00:');
+cursor_move(1 ,4);
+LCD_Send_character(num1);
+cursor_move(1 ,5);
+LCD_Send_character(num2);
+
+
+num3= KEYPAD_READ();
+LCD_clearScreen();
+cursor_move(1 ,1);
+LCD_Send_character('0');
+cursor_move(1 ,2);
+LCD_Send_character(num1);
+cursor_move(1 ,3);
+LCD_Send_character(':');
+cursor_move(1 ,3);
+LCD_Send_character(num2);
+cursor_move(1 ,4);
+LCD_Send_character(num3);
+
+
+
+num4= KEYPAD_READ();
+LCD_clearScreen();
+cursor_move(1 ,1);
+LCD_Send_character(num1);
+cursor_move(1 ,2);
+LCD_Send_character(num2);
+cursor_move(1 ,3);
+LCD_Send_character(':');
+cursor_move(1 ,4);
+LCD_Send_character(num3);
+cursor_move(1 ,5);
+LCD_Send_character(num4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
