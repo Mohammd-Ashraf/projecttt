6ce37eb68d5d3df2cc930cc785caed5e406c1475
Mohamed Youssef <1901194@eng.asu.edu.eg>
2022-05-18 02:00:07 +0200

    Update llcd.c

--- llcd.c
+++ llcd.c
@@ -1,67 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
-0
+#include "gpio.h"
 #include "lcd.h"
+#include "TIMER.h"
+#include "tm4c123gh6pm.h"
 
-void LCD_INIT(void)
-{
-    Port_init('B');
-    Port_init('E');
-    PORT_DIR('B', 0xFF);
-    PIN_DIR('E', 3, 1);  //E
-    PIN_DIR('E', 1,1 );  //Rs
-    PIN_DIR('E', 2,1 ); //Rw
-    PIN_WRITE('E', 2, 0);
-    lcd_command(0x38);
-    genericdelay(1);
-    /* Entry Mode Set  */
-    lcd_command(0x06);
-    genericdelay(1);
 
-    /* Display ON OFF Control */
-    lcd_command(0x0F);
-    genericdelay(1);
-
-    /* Clear Display */
-    lcd_command(0x01);
-    genericdelay(2);
-
-    /* Return Home  */
-    lcd_command(0x02);
-    genericdelay(2);
-}
 
 void lcd_command(unsigned char cmd)
 {
-    PIN_WRITE('B', cmd);
-    PIN_WRITE('E', 0x08);
+    PORT_WRITE('B', cmd);
+    PORT_WRITE('E', 0x04);
     // GPIO_PORTB_DATA_R=cmd;
     // GPIO_PORTE_DATA_R=0X04; // pin(B0-B7)
-    genericdelay(1);
-    PIN_WRITE('E', 0x00);
+    genericdelay(1,0);
+    PORT_WRITE('E', 0x00);
     // GPIO_PORTE_DATA_R=0; //Rs---->portE0,RW--->graound or portE1, E---->portE2
-    genericdelay(50);
+    genericdelay(50,0);
 
     return;
 }
+	
+	
+void LCD_INIT(void)
+{
+	
+		genericdelay(500,0);
+	
+    GPIO_Init(1);
+    GPIO_Init(2);
+    //PORT_DIR('B', 0xFF);
+	  GPIO_PORTB_DIR_R = 0xFF;
+//  	PIN_DIR('E', 3, 1);  //E
+//    PIN_DIR('E', 1,1 );  //Rs
+//    PIN_DIR('E', 2,1 ); //Rw
+		 GPIO_PORTE_DIR_R |= 0x07;
+	
+    //PIN_WRITE('E', 2, 0);
+	  //GPIO_PORTE_DATA_R &= ~(0x0);
+    lcd_command(0x38);
+    genericdelay(1,0);
+    /* Entry Mode Set  */
+    lcd_command(0x06);
+    genericdelay(1,0);
+
+    /* Display ON OFF Control */
+    lcd_command(0x0F);
+    genericdelay(1,0);
+
+    /* Clear Display */
+    lcd_command(0x01);
+    genericdelay(2,0);
+
+    /* Return Home  */
+    lcd_command(0x02);
+    genericdelay(2,0);
+}
+
+
+
 void send_char(unsigned char data) //-----> recive only one char
 {
-    PIN_WRITE(DATA_PORT, data);
-    // GPIO_PORTB_DATA_R=data;
-    PIN_WRITE(CTRL_PORT, 0x05);
-    // GPIO_PORTE_DATA_R=0X05;
-    genericdelay(1);
-    PIN_WRITE(CTRL_PORT, 0x01);
-    // GPIO_PORTE_DATA_R=0X01;
-    genericdelay(50);
+    //PORT_WRITE('B', data);
+     GPIO_PORTB_DATA_R=data;
+    //PORT_WRITE('E', 0x09);
+     GPIO_PORTE_DATA_R |= 0X05;
+    genericdelay(50,0);
+    //PORT_WRITE('E', 0x01);
+     GPIO_PORTE_DATA_R = 0X01;
+    genericdelay(50,0);
 
     return;
 }
 void send_String(unsigned char *data)
 {
-    while ((*data) != '0')
+    while ( *data )
     {
-        send_Char(*data);
+        send_char(*data);
         data++;
     }
 }
@@ -71,18 +86,18 @@ void LCD_Move_Cursor(unsigned char row, unsigned char col)
 
     if (row == 1)
     {
-        position = (FirstRow) + col - 1;
+        position = (0x80) + col - 1;
     }
     else if (row == 2)
     {
-        position = (SecondRow) + col - 1;
+        position = (0xC0) + col - 1;
     }
     else
     {
-        position = FirstRow;
+        position = 0x80;
     }
     lcd_command(position);
-    genericdelay(1);
+    genericdelay(1,0);
 }
 void LCD_COUNT_DOWN(unsigned char timer)
 {
@@ -90,13 +105,13 @@ void LCD_COUNT_DOWN(unsigned char timer)
     for (i = timer - 1; i >= 0; i--)
     {
         send_char(i);
-        genericdelay(1);
-        lcd_command(clear_display);
+        genericdelay(1,0);
+        lcd_command(0x01);
     }
 }
 
 void LCD_HOLD(unsigned char hold, unsigned char time)
 {
     send_char(hold);
-    genericdelay(time);
+    genericdelay(time,0);
 }
